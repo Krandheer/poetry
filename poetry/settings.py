0fa764e02817f37203b1895ba002bc7272127ae4
randheer <randheer.dakshana15@gmail.com>
2020-05-15 01:15:53 +0530

    some change to deploy

--- poetry/settings.py
+++ poetry/settings.py
@@ -1,11 +1,11 @@
-
 import os
-from .secret import *
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = '_&zh6e2j!)aq$)zbu6rdlz5a&z_onxp_@w$7j)tzd+kp4t@!v9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
