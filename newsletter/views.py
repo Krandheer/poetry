c7f06d1c47d8f2eeff131e2672fb249b8b038b7f
randheer <randheer.dakshana15@gmail.com>
2020-06-22 18:06:15 +0530

    newsletter

--- newsletter/views.py
+++ newsletter/views.py
@@ -3,25 +3,30 @@ from .forms import NewsUserForm
 from . models import NewsUsers
 from django.core.mail import send_mail
 
+
 def newsletter_subscribe(request):
-	if request.method == 'POST':
-		form = NewsUserForm(request.POST)
-		if form.is_valid():
-			instance = form.save(commit=False)
-			if NewsUsers.objects.filter(email=instance.email).exists():
-				print("you have subscribed already")
-			else:
-				instance.save()
-				form = NewsUserForm()
-			print('your email is added to our database')
-			# send_mail('Bhashadiaries blog ', 'welcome', 
-			# 			'bhaskarpandey003@gmail.com', [instance.email], fail_silently=False)
-	else:
-		form = NewsUserForm()
-	context = {'form':form}
 	template = "newsletter/subscribe.html"
+	return render(request, template, context={})
+
+# def newsletter_subscribe(request):
+# 	if request.method == 'POST':
+# 		form = NewsUserForm(request.POST)
+# 		if form.is_valid():
+# 			instance = form.save(commit=False)
+# 			if NewsUsers.objects.filter(email=instance.email).exists():
+# 				print("you have subscribed already")
+# 			else:
+# 				instance.save()
+# 				form = NewsUserForm()
+# 			print('your email is added to our database')
+# 			# send_mail('Bhashadiaries blog ', 'welcome', 
+# 			# 			'bhaskarpandey003@gmail.com', [instance.email], fail_silently=False)
+# 	else:
+# 		form = NewsUserForm()
+# 	context = {'form':form}
+# 	template = "newsletter/subscribe.html"
 	
-	return render(request, template, context)
+# 	return render(request, template, context)
 
 #I need to write a function which will pull my new content and sent it
 #to my subscriber as newsletter, email service provider will give me
